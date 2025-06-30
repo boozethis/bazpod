67cd75ca8aaa27f8c9b16f64790b89df5671cb93
boozethis <49071297+boozethis@users.noreply.github.com>
2025-06-30 22:32:26 +0100

    Update songs.js

--- songs.js
+++ songs.js
@@ -3,91 +3,91 @@ const songs = [
     "title": "Westie Ballerini",
     "image": "https://i.scdn.co/image/ab67616d0000b2738e858e54e203bf461e32fbb7",
     "spotify": "https://open.spotify.com/album/7baF2x8zMCfQ6piQRcqSec",
-    "youtube": "#",
-    "hyperfollow": "#"
+    "youtube": "https://youtu.be/hgOuXTilkHc",
+    "hyperfollow": "https://distrokid.com/hyperfollow/bazpod/westie-ballerini"
   },
   {
     "title": "Tarsiidae Pemalu",
     "image": "https://i.scdn.co/image/ab67616d0000b27367aeec045f33190448ddd7f7",
     "spotify": "https://open.spotify.com/album/4WBBoejNAYQz1bkFqWM5Dz",
-    "youtube": "#",
-    "hyperfollow": "#"
+    "youtube": "https://www.youtube.com/@bazp0d",
+    "hyperfollow": "https://distrokid.com/hyperfollow/bazpod/tarsiidae-pemalu"
   },
   {
     "title": "Macaccino Gelatone",
     "image": "https://i.scdn.co/image/ab67616d0000b273965c73d7756a84d243c9616b",
     "spotify": "https://open.spotify.com/album/0ErSR1h4KrxJwGWca1GnQp",
-    "youtube": "#",
-    "hyperfollow": "#"
+    "youtube": "https://www.youtube.com/@bazp0d",
+    "hyperfollow": "https://distrokid.com/hyperfollow/bazpod/macaccino-gelatone"
   },
   {
     "title": "Jerboa Dipodoa",
     "image": "https://i.scdn.co/image/ab67616d0000b273fdb9fb633078eb26c314caf7",
     "spotify": "https://open.spotify.com/album/0zHgVt4sCSNphfciCztD4c",
-    "youtube": "#",
-    "hyperfollow": "#"
+    "youtube": "https://www.youtube.com/@bazp0d",
+    "hyperfollow": "https://distrokid.com/hyperfollow/bazpod/jerboa-dipodoa"
   },
   {
     "title": "Colorino Tweetolini",
     "image": "https://i.scdn.co/image/ab67616d0000b2739ee956e8c2c7c9e40133a8ed",
     "spotify": "https://open.spotify.com/album/2AGtldyiAfN9pmVMHH9TPc",
-    "youtube": "#",
-    "hyperfollow": "#"
+    "youtube": "https://www.youtube.com/@bazp0d",
+    "hyperfollow": "https://distrokid.com/hyperfollow/bazpod/colorino-tweetolini"
   },
   {
     "title": "Antilocapra Ikaika",
     "image": "https://i.scdn.co/image/ab67616d0000b2732cceb39efa2b503b5aada024",
     "spotify": "https://open.spotify.com/album/3Odm2IKzxS9QXz8qDSFWpu",
-    "youtube": "#",
-    "hyperfollow": "#"
+    "youtube": "https://www.youtube.com/@bazp0d",
+    "hyperfollow": "https://distrokid.com/hyperfollow/bazpod/antilocapra-ikaika"
   },
   {
     "title": "Zazi the Ant Queen",
     "image": "https://i.scdn.co/image/ab67616d0000b273e96f33322912f5ee21375f1d",
     "spotify": "https://open.spotify.com/album/5qoH6PLp0G4BmMuMtGrWt5",
-    "youtube": "#",
-    "hyperfollow": "#"
+    "youtube": "https://youtu.be/rWos0iqrjSo",
+    "hyperfollow": "https://distrokid.com/hyperfollow/bazpod/zazi-the-ant-queen"
   },
   {
     "title": "Quokka Mocha",
     "image": "https://i.scdn.co/image/ab67616d0000b273b8f77b498edbb58078212039",
     "spotify": "https://open.spotify.com/album/3ldyOTT9WBUp4yFotsJYAd",
-    "youtube": "#",
-    "hyperfollow": "#"
+    "youtube": "https://youtu.be/VKX9BjoBF2U",
+    "hyperfollow": "https://distrokid.com/hyperfollow/bazpod/quokka-mocha"
   },
   {
     "title": "Pigini Bikini",
     "image": "https://i.scdn.co/image/ab67616d0000b273023f357f18607ad69d3e5e8d",
     "spotify": "https://open.spotify.com/album/6tKkgjBrfMZvbwne91dRGR",
-    "youtube": "#",
-    "hyperfollow": "#"
+    "youtube": "https://youtu.be/EffECcVZP2w",
+    "hyperfollow": "https://distrokid.com/hyperfollow/bazpod/pigini-bikini"
   },
   {
     "title": "Kitty Coconutty",
     "image": "https://i.scdn.co/image/ab67616d0000b273ab93948887d70acd6b562b7b",
     "spotify": "https://open.spotify.com/album/2DEjFPw76BmRPbC74S9zaD",
-    "youtube": "#",
-    "hyperfollow": "#"
+    "youtube": "https://youtu.be/OBZ5msVYTBw",
+    "hyperfollow": "https://distrokid.com/hyperfollow/bazpod/kitty-coconutty"
   },
   {
     "title": "Crannchu na Coille",
     "image": "https://i.scdn.co/image/ab67616d0000b2732909b781f3e9da43e00376c3",
     "spotify": "https://open.spotify.com/album/2TXC3eU1j9fb93ncMzTbwD",
-    "youtube": "#",
-    "hyperfollow": "#"
+    "youtube": "https://youtube.com/shorts/vl19Tzk_6Mo?feature=share",
+    "hyperfollow": "https://distrokid.com/hyperfollow/bazpod/crannchu-na-coille"
   },
   {
     "title": "Axolotti Springalotti",
     "image": "https://i.scdn.co/image/ab67616d0000b2733f8f5fb9f97f6ad17e1d1c60",
     "spotify": "https://open.spotify.com/album/1tJtXbEgtjLzO1CtPtpQIv",
-    "youtube": "#",
-    "hyperfollow": "#"
+    "youtube": "https://youtu.be/Hy5l5r0kpBk",
+    "hyperfollow": "https://distrokid.com/hyperfollow/bazpod/axolotti-springalotti"
   },
   {
     "title": "Aleena Jalapeña",
     "image": "https://i.scdn.co/image/ab67616d0000b273695ef9a291b22aa8bd241681",
     "spotify": "https://open.spotify.com/album/7LxwQV3puQZPq1nXp4cLmT",
-    "youtube": "#",
-    "hyperfollow": "#"
+    "youtube": "https://youtu.be/yQtbKMp79dE",
+    "hyperfollow": "https://distrokid.com/hyperfollow/bazpod/aleena-jalapea"
   }
 ];
